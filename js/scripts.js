b47b20e124d435e3aa0fd2c50a178aa44353142e
Garrett Biernat and Kellie Olson <garrett@>
2016-03-23 13:23:25 -0700

    add the ability to handle decimals

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ var perc = function(num1, num2) {
 }
 var pow = function(num1, num2) {
 	x = num1;
-	for (i = 0; i < num2; i++){
+	for (i = 1; i < num2; i++){
 
   	num1 = num1 * x;
   }
@@ -27,50 +27,50 @@ $(function(event){
 
 	
 	$("button#add").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = add(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+" + "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
 	$("button#sub").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = sub(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+" - "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
 	$("button#mult").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = mult(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+" * "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
 	$("button#div").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = div(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+" / "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
 	$("button#pow").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = pow(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+" to the power of "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
 	$("button#perc").click(function(event){
-		var num1 = parseInt($("#num1").val());
-    	var num2 = parseInt($("#num2").val());
+		var num1 = parseFloat($("#num1").val());
+    	var num2 = parseFloat($("#num2").val());
 		var answer = perc(num1,num2);
-		$("ol").prepend("<li>"+answer+"</li>");
+		$("ol").prepend("<li>"+num1+"% of "+num2+" = "+answer+"</li>");
 		event.preventDefault();
 
 	});
